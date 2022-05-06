f249028201d47982b5c6e823ca574800234974c2
Matej Husek <czmathew@gmail.com>
2022-05-07 00:32:53 +0200

    calculator page added

--- TBillStakingDashboardWeb/wwwroot/js/dashboard.js
+++ TBillStakingDashboardWeb/wwwroot/js/dashboard.js
@@ -61,10 +61,11 @@ function fetchWalletData() {
                 var tbill15x = parseFloat(val['tbill15x']).toFixed(2);
                 var tbill2x = parseFloat(val['tbill2x']).toFixed(2);
                 var tbill3x = parseFloat(val['tbill3x']).toFixed(2);
+                var tbill4x = parseFloat(val['tbill4x']).toFixed(2);
                 var tfuel = parseFloat(val['tfuel']).toFixed(2);
                 if (i == 1) {
                     $("#tvl").html(tvl);
-                    $("#activTbill").html(parseFloat(tbill1x) + parseFloat(tbill125x) + parseFloat(tbill15x) + parseFloat(tbill2x) + parseFloat(tbill3x));
+                    $("#activTbill").html(parseFloat(tbill1x) + parseFloat(tbill125x) + parseFloat(tbill15x) + parseFloat(tbill2x) + parseFloat(tbill3x) + parseFloat(tbill4x));
                     $("#activeTfuel").html(tfuel);
                     nft125xlevel = tbill125x;
                     nft15xlevel = tbill15x;
---
+++ TBillStakingDashboardWeb/wwwroot/js/pageCalculator.js
@@ -0,0 +1,79 @@
+﻿$(document).ready(function () {
+    calculate();
+
+    $("#calculate").click(function (event) {
+        event.preventDefault();
+        calculate();
+    });
+});
+
+
+
+function calculate() {
+    var firstInvest = parseFloat($("#firstInvestment").val());
+    var monthlyInvest = parseFloat($("#monthlyInvestment").val());
+    var multi2x = parseFloat($("#2xMulti").val());
+    var tfuelPrice = parseFloat($("#tfuelPrice").val());
+    var tbillPrice = parseFloat($("#tbillPrice").val());
+    var compound = parseFloat($("#compound").val());
+
+    //alert(compound);
+
+    var years = 10;
+    var yearsInLP = [];
+
+    for (let i = 0; i < years; i++) {
+        if (i == 0) {
+            var calcTbill = firstInvest / 2 / tbillPrice;
+            var calcTfuel = firstInvest / 2 / tfuelPrice;
+            var usedMulti2x = (multi2x > calcTbill) ? 1 : (1 - ((calcTbill - multi2x) / calcTbill));
+            var apr = (0.75 * usedMulti2x) + (0.5 * (1 - usedMulti2x));
+            var apy = Math.pow((1 + (apr / compound)), (compound)) - 1;
+            let year = new yearInLP(firstInvest, 0, calcTfuel, calcTbill, usedMulti2x, apr, apy);
+            yearsInLP.push(year);
+        } else {
+            var prevYear = yearsInLP[i - 1];
+            var x = prevYear.apy;
+            var val = (prevYear.val + (12 * monthlyInvest)) * (1 + prevYear.apy);
+            var netGain = val - prevYear.val;
+            var calcTbill = val / 2 / tbillPrice;
+            var calcTfuel = val / 2 / tfuelPrice;
+            var usedMulti2x = (multi2x > calcTbill) ? 1 : (1 - ((calcTbill - multi2x) / calcTbill));
+            var apr = (0.75 * usedMulti2x) + (0.5 * (1 - usedMulti2x));
+            var apy = Math.pow((1 + (apr / compound)), (compound)) - 1;
+            let year = new yearInLP(val, netGain, calcTfuel, calcTbill, usedMulti2x, apr, apy);
+            yearsInLP.push(year);
+        }
+    }
+
+    $('#calcTable > tbody:last-child').empty();
+    $.each(yearsInLP, function (i, val) {
+
+        var valu = (val.val).toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+        var netGain = (val.netGain).toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+        var calcTfuel = (val.calcTfuel).toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+        var calcTbill = (val.calcTbill).toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+        var multi2x = (val.usedMulti2x * 100).toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+        var apr = (val.apr * 100).toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+        var apy = (val.apy*100).toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+
+        $('#calcTable > tbody:last-child').append('<tr><td>' + i + '</td><td class="text-end">$' + valu + '</td><td class="text-end">$' + netGain + '</td><td class="text-end">' + calcTfuel + '</td><td class="text-end">' + calcTbill + '</td><td class="text-end">' + multi2x + '%</td><td class="text-end">' + apr + '%</td><td class="text-end">' + apy + '%</td></tr>');
+
+    });
+
+      
+   
+
+}
+
+class yearInLP {
+    constructor(val, netGain, calcTfuel, calcTbill, usedMulti2x, apr, apy) {
+        this.val = val;
+        this.netGain = netGain;
+        this.calcTfuel = calcTfuel;
+        this.calcTbill = calcTbill;
+        this.usedMulti2x = usedMulti2x;
+        this.apr = apr;
+        this.apy = apy;
+    }
+}
